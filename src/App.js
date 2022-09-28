aa1c2590bbe831bda223d9b34549066193228adc
Gary Whitford <ssrdunc@gmail.com>
2022-09-28 19:12:50 -0400

    last branch commit before first merge to main

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { Routes, Route } from "react-router-dom" ; 
-import AskQuestion from "./ask-question";
-import PostAnswer from './post-answers'
+
 function App() {
   return (
-  <div>
-      <div>
-        <AskQuestion token={'2dc43cc797e571669d4ee81fb0fbbea3bb8de2c1'}/>
-      </div>
-      <div><PostAnswer token={'2dc43cc797e571669d4ee81fb0fbbea3bb8de2c1'} /> 
-      </div>
-      {/* <Routes>
-        <Route path="ask-question" element={<AskQuestion />} />
-      </Routes> */}
-  </div>
+
 )}
 
 export default App;
